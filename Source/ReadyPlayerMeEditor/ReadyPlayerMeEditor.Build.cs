ceb3cfad973d707d2e4e545b6438c1bfe5661e94
Gevorg <Gevorg.ad@gmail.com>
2023-04-17 13:02:22 +0200

    Release 1.1.0
    Release fixes for sdk release v1.1
    
    ## Description
    
    -   Set UnityBuild flag to false to fix the build issues.
    -   Made the ReadyPlayerMe plugin installed.
    -   Updated changelog and readme.
    
    ## Changes
    
    #### Updated
    
    -   Set UnityBuild flag to false to fix the build issues.
    -   Made the ReadyPlayerMe plugin installed.
    -   Updated changelog and readme.

--- Source/ReadyPlayerMeEditor/ReadyPlayerMeEditor.Build.cs
+++ Source/ReadyPlayerMeEditor/ReadyPlayerMeEditor.Build.cs
@@ -7,6 +7,7 @@ public class ReadyPlayerMeEditor : ModuleRules
 	public ReadyPlayerMeEditor(ReadOnlyTargetRules Target) : base(Target) 
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		bUseUnity = false;
 
 		PublicIncludePaths.AddRange(
 			new string[] {
